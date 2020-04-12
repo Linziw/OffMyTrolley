4ea4897ad36cddb52038a2b48831386de497e528
Lindsey Wells <github email address>
2020-04-12 20:29:35 +0100

    edit migrations and add associations

--- off-my-trolley-api/app/models/trolley.rb
+++ off-my-trolley-api/app/models/trolley.rb
@@ -1,2 +1,3 @@
 class Trolley < ApplicationRecord
+  belongs_to :User
 end
--- off-my-trolley-api/db/migrate/20200412095601_create_trolleys.rb
+++ off-my-trolley-api/db/migrate/20200412095601_create_trolleys.rb
@@ -1,11 +1,11 @@
 class CreateTrolleys < ActiveRecord::Migration[6.0]
   def change
     create_table :trolleys do |t|
-      t.string :date
-      t.string :time
+      t.date :date
+      t.time :time
       t.string :supermarket
-      t.string :space
-      t.string :user_id
+      t.integer :space
+      t.integer :user_id
 
       t.timestamps
     end
