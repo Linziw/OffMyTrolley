e2b909908ef1834fe05137a4ff808a265f6f63a5
Lindsey Wells <github email address>
2020-04-13 19:44:36 +0100

    hide buttons once clicked

--- frontend/index.js
+++ frontend/index.js
@@ -1,12 +1,16 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     let login_button = document.getElementById("login-button");
+    let signup_button = document.getElementById("signup-button");
     login_button.addEventListener("click", function(e) {
         alert("login has been clicked!")
+        login_button.classList.add("hidden")
+        signup_button.classList.add("hidden")
     });
 
-    let signup_button = document.getElementById("signup-button");
     signup_button.addEventListener("click", function(e) {
         alert("signup has been clicked!")
+        login_button.classList.add("hidden")
+        signup_button.classList.add("hidden")
     });
 
 
