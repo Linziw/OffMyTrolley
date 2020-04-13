9c0359398ed3489a35d0ce55139da9783da7b89a
Lindsey Wells <github email address>
2020-04-13 16:31:23 +0100

    add show method

--- off-my-trolley-api/app/controllers/trolleys_controller.rb
+++ off-my-trolley-api/app/controllers/trolleys_controller.rb
@@ -3,4 +3,9 @@ class TrolleysController < ApplicationController
     trolleys = Trolley.all
     render json: TrolleySerializer.new(trolleys).to_serialized_json
   end
+
+  def show
+    trolley = Trolley.find_by(id: params[:id])
+    render json: TrolleySerializer.new(trolley).to_serialized_json
+  end
 end
