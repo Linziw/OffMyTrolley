3a4621d4fc430d6f2e773562b5bd89cd33ab563b
Lindsey Wells <github email address>
2020-04-13 19:49:14 +0100

    remove hidden class from forms once button is clicked

--- frontend/index.js
+++ frontend/index.js
@@ -1,16 +1,21 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     let login_button = document.getElementById("login-button");
     let signup_button = document.getElementById("signup-button");
+    let login_form = document.getElementById("login-form");
+    let signup_form = document.getElementById("signup-form");
+
     login_button.addEventListener("click", function(e) {
         alert("login has been clicked!")
         login_button.classList.add("hidden")
         signup_button.classList.add("hidden")
+        login_form.classList.remove("hidden")
     });
 
     signup_button.addEventListener("click", function(e) {
         alert("signup has been clicked!")
         login_button.classList.add("hidden")
         signup_button.classList.add("hidden")
+        signup_form.classList.remove("hidden")
     });
 
 
