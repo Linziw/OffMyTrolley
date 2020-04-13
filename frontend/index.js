048f5265a406e8fbaffc4aba0e9a52c878eef2ac
Lindsey Wells <github email address>
2020-04-13 19:40:19 +0100

    add event listeners to login and signup buttons

--- frontend/index.js
+++ frontend/index.js
@@ -1,3 +1,13 @@
 window.addEventListener('DOMContentLoaded', (event) => {
-    console.log('Its loaded and js is working');
-});
+    let login_button = document.getElementById("login-button");
+    login_button.addEventListener("click", function(e) {
+        alert("login has been clicked!")
+    });
+
+    let signup_button = document.getElementById("signup-button");
+    signup_button.addEventListener("click", function(e) {
+        alert("signup has been clicked!")
+    });
+
+
+})
