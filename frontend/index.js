2bb4b7717847c85dc7d9e605de1ee131804d2961
Lindsey Wells <github email address>
2020-04-13 20:55:39 +0100

    split up sign up and log in events

--- frontend/index.js
+++ frontend/index.js
@@ -1,32 +1,46 @@
 window.addEventListener('DOMContentLoaded', (event) => {
-    let login_button = document.getElementById("login-button");
-    let signup_button = document.getElementById("signup-button");
-    let login_form = document.getElementById("login-form");
-    let signup_form = document.getElementById("signup-form");
+    loginEvents()
+    signupEvents()
 
-    login_button.addEventListener("click", function(e) {
-        login_button.classList.add("hidden");
-        signup_button.classList.add("hidden");
-        login_form.classList.remove("hidden");
 
-        let login_submit = document.getElementById("login-submit");
-        login_submit.addEventListener("click", (event) => {
-            event.preventDefault()
-        })
 
-    });
+    function loginEvents() {
+        let login_button = document.getElementById("login-button");
+        let login_form = document.getElementById("login-form");
+        let signup_button = document.getElementById("signup-button");
 
-    signup_button.addEventListener("click", function(e) {
-        login_button.classList.add("hidden")
-        signup_button.classList.add("hidden")
-        signup_form.classList.remove("hidden")
+        login_button.addEventListener("click", function(e) {
+            login_button.classList.add("hidden");
+            signup_button.classList.add("hidden");
+            login_form.classList.remove("hidden");
 
-        let signup_submit = document.getElementById("signup-submit");
-        signup_submit.addEventListener("click", (event) => {
-            event.preventDefault()
-        })
-    });
+            let login_submit = document.getElementById("login-submit");
+            login_submit.addEventListener("click", (event) => {
+                fetch(login_form.action, { method: 'post', body: new FormData(login_form) });
+                console.log('We send post asynchronously (AJAX)');
+                event.preventDefault();
+            })
+        });
+    }
 
+    function signupEvents() {
+        let signup_button = document.getElementById("signup-button");
+        let signup_form = document.getElementById("signup-form");
+        let login_button = document.getElementById("login-button");
+
+        signup_button.addEventListener("click", function(e) {
+            login_button.classList.add("hidden")
+            signup_button.classList.add("hidden")
+            signup_form.classList.remove("hidden")
+
+            let signup_submit = document.getElementById("signup-submit");
+            signup_submit.addEventListener("click", (event) => {
+                fetch(signup_form.action, { method: 'post', body: new FormData(signup_form) });
+                console.log('We send post asynchronously (AJAX)');
+                event.preventDefault();
+            })
+        });
+    }
 
 
 })
